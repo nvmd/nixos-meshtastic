136f9c39009e1add1be3b63d9dfd6ba4bc10735e
Sergey Kazenyuk <kazenyuk@pm.me>
2025-02-28 15:03:32 +0300

    meshtasticd: update to 2.5.18.89ebafc
    The latest version with .deb being provided on Github

--- pkgs/meshtasticd.nix
+++ pkgs/meshtasticd.nix
@@ -2,7 +2,9 @@
 , fetchurl
 , autoPatchelfHook
 , dpkg
+, libusb1
 , libgpiod_1
+, i2c-tools
 , libyaml-cpp
 , ulfius
 , orcania
@@ -15,20 +17,20 @@ let
   srcVariant = {
     x86_64-linux = {
       arch = "amd64";
-      hash = "sha256-9s63FdXa+opXm82qsLX9t4JEFmbQ65YShXCrrxLdqG0=";
+      hash = "sha256-qTeze73sHXA1ASgRbyg3aCL57ERgWpM7aaXjWa2aiVg=";
     };
     aarch64-linux = {
       arch = "arm64";
-      hash = "sha256-vuW+yjOtQrqX9UWPMtWtJaAwUwB/2z/lbQPp3HGnwm4=";
+      hash = "sha256-Efwso6/werSFGT1KuzANDNkb2VdYeoPcLyOropq8F4o=";
     };
     armv7l-linux = {
       arch = "armhf";
-      hash = "sha256-6HaFGMXsevAi/hmmxhTAqNMWF4r7S6dIn09sr9D0adg=";
+      hash = "sha256-YJeitQOHOVAWHCTSU5pTyLJvwo7QMwOoSpHlk988vVY=";
     };
   }.${stdenv.hostPlatform.system} or unsupported;
 in stdenv.mkDerivation (finalAttrs: {
   pname = "meshtasticd";
-  version = "2.5.15.79da236";
+  version = "2.5.18.89ebafc";
 
   src = let
     baseUrl = "https://github.com/meshtastic/firmware/releases/download";
@@ -46,7 +48,9 @@ in stdenv.mkDerivation (finalAttrs: {
   # https://github.com/meshtastic/firmware/blob/master/Dockerfile
   # https://packages.debian.org/search?suite=bookworm&keywords=keywords
   buildInputs = [
+    libusb1     # nixpkgs: 1.0.27, debian: 1.0.26
     libgpiod_1  # nixpkgs: 1.6.4, debian: 1.6.3
+    i2c-tools   # nixpkgs: 4.4, debian: 4.3 (libi2c)
     libyaml-cpp # nixpkgs: -, debian: 0.7.0
     ulfius      # nixpkgs: -, debian: 2.7.13
     orcania     # nixpkgs: 2.3.3, debian: 2.3.2
